08c59904502e61dc2f4a971fdf30c51c8ed0184e
Max Block <maxblock@pm.me>
2021-12-13 12:19:29 +0300

    update

--- mb_std/__init__.py
+++ mb_std/__init__.py
@@ -7,4 +7,4 @@ from .net import check_port  # noqa: F401
 from .result import Result  # noqa: F401
 from .str import number_with_separator, str_to_list  # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"

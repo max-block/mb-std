9c8fd3f5a7631b26cbc5a6f9013eef8037cabf1b
Max Block <maxblock@pm.me>
2022-03-28 12:56:58 +0300

    update

--- mb_std/__init__.py
+++ mb_std/__init__.py
@@ -8,4 +8,4 @@ from .net import check_port  # noqa: F401
 from .result import Result  # noqa: F401
 from .str import number_with_separator, str_to_list  # noqa: F401
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from mb_std.mongo import MongoConnection
 
 @pytest.fixture
 def mongo_database() -> Database:
-    conn = MongoConnection.connect("mongodb://localhost/mb-commons__test")
+    conn = MongoConnection.connect("mongodb://localhost/mb-std__test")
     conn.client.drop_database(conn.database)
 
-    conn = MongoConnection.connect("mongodb://localhost/mb-commons__test")
+    conn = MongoConnection.connect("mongodb://localhost/mb-std__test")
     return conn.database
